5ad48fc026d50a2f3e5652c29053ebc99380a829
KuyaMoEd <31286911+dipaaapi@users.noreply.github.com>
2022-03-29 21:49:28 +0800

    initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+// import TheWelcome from "@/components/TheWelcome.vue";
+</script>
+
+<template>
+  <div class="mainav">
+    <main>
+      <TheWelcome />
+    </main>
+    <div class="">
+      <h3 class="">Welcome Page</h3>
+    </div>
+  </div>
+</template>
+
+<style>
+  .mainav {
+    display: flex;
+  }
+  h3 {
+    font-size: 2.5em;
+    color:white;
+  }
+  body {
+    
+  }
+</style>
